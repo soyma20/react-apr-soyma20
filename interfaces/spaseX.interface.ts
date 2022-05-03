35302fc8bc3d71a390facd3d9a79501561905eb7
soyma20 <slaviksoyma2014@gmail.com>
2022-05-03 13:56:28 +0300

    MY HW

---
+++ interfaces/spaseX.interface.ts
@@ -0,0 +1,37 @@
+export interface Ilaunch {
+    mission_name:string;
+    launch_date_local:string;
+    launch_site:{
+        site_name_long:string;
+    };
+    links:{
+        article_link:null;
+        video_link:string;
+    };
+    rocket:{
+        rocket_name:string;
+        first_stage:{
+            cores:[
+                {
+                    flight:number;
+                    core:{
+                        reuse_count:number;
+                        status:unknown
+                    }
+                }
+            ]
+        };
+
+    };
+    second_stage:{
+        payloads:[
+            {
+                payload_type:string;
+                payload_mass_kg:number;
+                payload_mass_lbs:number;
+            }
+        ]
+}
+
+}
+
